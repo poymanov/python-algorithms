dc957cbbfa854c6cb56236c1e8f562c010df8aab
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-09 23:39:38 +0300

    Fix Task 10

--- 10/AssocArray.py
+++ 10/AssocArray.py
@@ -1,18 +1,51 @@
 class AssocArray:
-	def __init__(self):		
-		self.slots = []
-		self.values = []
+	def __init__(self, sz, stp):
+		self.size = sz
+		self.step = stp
+		self.slots = [None] * self.size
+		self.values = [None] * self.size
 
+	def hash_fun(self, value):
+		value = str(value)
+
+		bytes_sum = 0
+
+		for symbol in value:
+			bytes_sum += ord(symbol)
+
+		return bytes_sum % self.size
+
+	def seek_slot(self, value):
+		base_index = self.hash_fun(value)
+		index = base_index
+		first_round = True
+
+		while self.slots[index] is not None:
+			index += self.step
+
+			if first_round:
+				if index >= self.size:
+					first_round = False
+					index = 0
+			else:
+				if index >= base_index:
+					return None
+
+		return index
+		
 	def put(self, key, value):
-		self.slots.append(key)
-		self.values.append(value)
+		index = self.seek_slot(key)
+		self.slots[index] = key
+		self.values[index] = value
 
 	def is_key(self, key):
-		return key in self.slots
+		index = self.hash_fun(key)
+		return self.slots[index] is not None
 
 	def get(self, key):
-		result = None
+		index = self.hash_fun(key)
 
-		if self.is_key(key): result = self.values[self.slots.index(key)]
-
-		return result
+		if self.slots[index] is not None:		
+			return self.values[index]
+		else:
+			return None
--- 10/tests.py
+++ 10/tests.py
@@ -2,25 +2,29 @@ import unittest
 from AssocArray import AssocArray
 
 class AssocArrayCase(unittest.TestCase):
+	def setUp(self):
+		self.hash_size = 19
+		self.hash_step = 3
+
 	def test_add_new_value(self):
-		assoc_array = AssocArray()
+		assoc_array = AssocArray(self.hash_size, self.hash_step)
 		assoc_array.put('key', 'value')
 
-		self.assertIsNotNone(assoc_array.slots[0])
-		self.assertEqual('key', assoc_array.slots[0])
+		self.assertIsNotNone(assoc_array.slots[6])
+		self.assertEqual('key', assoc_array.slots[6])
 
-		self.assertIsNotNone(assoc_array.values[0])
-		self.assertEqual('value', assoc_array.values[0])
+		self.assertIsNotNone(assoc_array.values[6])
+		self.assertEqual('value', assoc_array.values[6])
 
 	def test_check_key_exists(self):
-		assoc_array = AssocArray()
+		assoc_array = AssocArray(self.hash_size, self.hash_step)
 		assoc_array.put('key', 'value')
 
 		self.assertTrue(assoc_array.is_key('key'))
 		self.assertFalse(assoc_array.is_key('key2'))
 
 	def test_get_value_by_key(self):
-		assoc_array = AssocArray()
+		assoc_array = AssocArray(self.hash_size, self.hash_step)
 		assoc_array.put('key', 'value')
 
 		self.assertEqual('value', assoc_array.get('key'))
