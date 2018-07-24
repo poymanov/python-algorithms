8d990c608de100208a4143e33639d106b5fb2233
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-24 20:08:09 +0300

    Modify Task 16

---
+++ 16/Tree.py
@@ -0,0 +1,90 @@
+class Tree:
+	def __init__(self, depth, root):
+		size = self.get_array_size(depth)
+		self.data = [None] * size
+		self.data[0] = root
+
+	def get_left_index(self, parent_index):
+		return 2 * parent_index + 1
+
+	def get_right_index(self, parent_index):
+		return 2 * parent_index + 2
+
+	def get_array_size(self, depth):		
+		childs = None
+		array_size = 0
+
+		for i in range(depth):				
+			if childs == None:
+				childs = 1
+			else:
+				childs *=2
+
+			array_size += childs
+
+		return array_size	
+
+	def add_left(self, parent, child):
+		if not parent in self.data:
+			raise 'Unknown parent'			
+
+		parent_index = self.data.index(parent)	
+
+		left_index = self.get_left_index(parent_index)
+		right_index = self.get_right_index(parent_index)
+
+		try:
+			left_value = self.data[left_index]	
+		except:
+			raise 'Failed to found slot for child'
+
+		if parent < child:
+			raise ValueError('Parent value less then child value')		
+
+		right_value = self.data[right_index]	
+
+		if right_value and child > right_value:
+			raise ValueError('Right child key less then node key')		
+
+		self.data[left_index] = child			
+
+	def add_right(self, parent, child):
+		if not parent in self.data:
+			raise 'Unknown parent'			
+
+		parent_index = self.data.index(parent)
+		right_index = self.get_right_index(parent_index)
+
+		try:
+			right_value = self.data[right_index]	
+		except:
+			raise 'Failed to found slot for child'
+
+		if parent > child:
+			raise ValueError('Parent value biggest then child value')				
+
+		self.data[right_index] = child
+
+	def search(self, value, parent = None):
+		if parent == None:
+			parent = self.data[0]
+
+		parent_index = self.data.index(parent)
+
+		if parent == value:
+			return parent_index		
+		else:
+			if value < parent:			
+		 		index = self.get_left_index(parent_index)
+			else:
+				index = self.get_right_index(parent_index)	 
+
+			try:
+				next_value = self.data[index]
+
+				if next_value == None:
+					return -12
+				else:		
+					return self.search(value, self.data[index])
+			except:
+				return None	
--- 16/tests.py
+++ 16/tests.py
@@ -1,62 +1,112 @@
 import unittest
-from Tree2.Node import Node
-from Tree2.Tree import Tree
-from functions import *
+from Tree import Tree
 
 class TreeToArrayCase(unittest.TestCase):
 	def setUp(self):
-		node1 = Node(None, 50)
-		node2 = Node(None, 25)
-		node3 = Node(None, 75)
-		node4 = Node(None, 37)
-		node5 = Node(None, 62)
-		node6 = Node(None, 84)
-		node7 = Node(None, 31)
-		node8 = Node(None, 43)
-		node9 = Node(None, 55)
-		node10 = Node(None, 92)
+		tree = Tree(4, 50)
+		tree.add_left(50, 25)
+		tree.add_right(50, 75)
 
-		tree = Tree(4)
-		tree.root = node1
+		tree.add_right(25, 37)
+		tree.add_left(37, 31)
+		tree.add_right(37, 43)
 
-		node1.add_left(node2)
-		node1.add_right(node3)
-	
-		node2.add_right(node4)
-		node4.add_left(node7)
-		node4.add_right(node8)
+		tree.add_left(75, 62)
+		tree.add_left(62, 55)
 
-		node3.add_left(node5)
-		node5.add_left(node9)
-
-		node3.add_right(node6)
-		node6.add_right(node10)
+		tree.add_right(75, 84)
+		tree.add_right(84, 92)
 
 		self.tree = tree
-		self.array = create_array_from_tree(self.tree)
 
-	def test_get_array_size(self):
-		tree = Tree(2)		
-		self.assertEqual(3, tree.get_array_size())
+	def test_init_tree(self):
+		tree = Tree(4, 50)
 
-		tree = Tree(3)		
-		self.assertEqual(7, tree.get_array_size())
+		result_data = [None] * 15
+		result_data[0] = 50
 
-		tree = Tree(4)		
-		self.assertEqual(15, tree.get_array_size())
+		self.assertEqual(result_data, tree.data)
 
-	def test_create_array_from_tree(self):		
-		result_array = [50, 25, 75, None, 37, 62, 84, None, None, 31, 43, 55, None, None, 92]	
-		self.assertEqual(result_array, self.array)
+	def test_get_left_index(self):
+		tree = Tree(4, 50)
+		self.assertEqual(1, tree.get_left_index(0))
+
+	def test_get_right_index(self):
+		tree = Tree(4, 50)
+		self.assertEqual(2, tree.get_right_index(0))		
+
+	def test_add_left_child_failed(self):	
+		tree = Tree(4, 50)
+
+		with self.assertRaises(Exception):
+			tree.add_left(99, 10)
+
+		tree = Tree(1, 50)			
+
+		with self.assertRaises(Exception):
+			tree.add_left(50, 10)
+
+		tree = Tree(4, 50)
+
+		with self.assertRaises(Exception):
+			tree.add_left(50, 100)	
+
+		tree = Tree(4, 50)
+		tree.data[2] = 5
+
+		with self.assertRaises(Exception):
+			tree.add_left(50, 10)		
+
+	def test_add_left_child_success(self):
+		tree = Tree(4, 50)
+		tree.add_left(50, 25)
+
+		result_data = [None] * 15
+		result_data[0] = 50
+		result_data[1] = 25
+
+		self.assertEqual(result_data, tree.data)
+
+	def test_add_right_child_failed(self):
+		tree = Tree(4, 50)
+
+		with self.assertRaises(Exception):
+			tree.add_right(99, 10)
+
+		tree = Tree(1, 50)			
+
+		with self.assertRaises(Exception):
+			tree.add_right(50, 10)			
+
+		tree = Tree(4, 50)
+
+		with self.assertRaises(Exception):
+			tree.add_right(50, 2)	
+
+	def test_add_right_child_success(self):
+		tree = Tree(4, 50)
+		tree.add_left(50, 25)
+		tree.add_right(50, 75)
+
+		result_data = [None] * 15
+		result_data[0] = 50
+		result_data[1] = 25
+		result_data[2] = 75
+
+		self.assertEqual(result_data, tree.data)
+
+	def test_create_tree(self):
+		result_data = [50, 25, 75, None, 37, 62, 84, None, None, 31, 43, 55, None, None, 92]
+		self.assertEqual(result_data, self.tree.data)
 
 	def test_search_missing_key(self):
-		self.assertIsNone(search(self.tree, self.array, 100))
+		self.assertIsNone(self.tree.search(100))
 
 	def test_search_exists_key(self):
-		self.assertEqual(6, search(self.tree, self.array, 84))
+		self.assertEqual(6, self.tree.search(84))
 
 	def test_search_missing_acceptable_key(self):
-		self.assertEqual(-12, search(self.tree, self.array, 21))
+		self.assertEqual(-12, self.tree.search(21))
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)		
